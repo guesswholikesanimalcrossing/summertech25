16551009b09613444eba65aa3fffa27871198c4d
guesswholikesanimalcrossing <thebeargirl31@gmail.com>
2025-07-25 14:03:00 +0000

    added to the game, hungry flame boi

--- exist.py
+++ exist.py
@@ -1,6 +1,7 @@
 from random import randint
 friends=randint(0,3)
 inventory=[]
+gasagain=("Y")
 moneyrandom=randint(0,10000)
 print("you have",moneyrandom,"money.")
 print("You have",friends,"friend(s).")
@@ -27,13 +28,25 @@ elif nuggetsyesno==("Y"):
     friends=friends+1
     print("You now have",friends,"friend(s).")
 print("You spot a gas station on the horizon. You head in.")
-gasbuy=["oreos","soda can","gas","energy drink"]
-gasbuyprices=[5,3,15,5]
-print("You see many things. What do you buy?")
-for i in range(len(gasbuy)):
-    print((i),gasbuy[i])
-buyatstation=int(input("Please type the number of the thing you would like to buy. "))
-for i, thing in enumerate(gasbuy):
-    if gasbuy[buyatstation]==thing:
-        moneyrandom=moneyrandom-gasbuyprices[i]
-print(moneyrandom)
+while gasagain==("Y"):
+    gasbuy=["oreos","soda can","gas","energy drink"]
+    gasbuyprices=[5,3,15,5]
+    print("You see many things. What do you buy?")
+    for i in range(len(gasbuy)):
+        print((i),gasbuy[i])
+    buyatstation=int(input("Please type the number of the thing you would like to buy. "))
+    for i, thing in enumerate(gasbuy):
+        if gasbuy[buyatstation]==thing:
+            if moneyrandom<gasbuyprices[buyatstation]:
+                print("no money for that stuff... try restarting the game...")
+            else:
+                moneyrandom=moneyrandom-gasbuyprices[i]
+                print("You have",moneyrandom,"money.")
+                print("You have purchased",gasbuy[buyatstation])
+                inventory.append(gasbuy[buyatstation])
+                print("inventory check: You have",inventory)
+                gasagain=input("Would you like to buy more? Please type Y or N.")
+print("You walk out of the gas station. There is a flame there, crying.")
+whatswrongfire=input("Ask what's wrong? Type Y or N.")
+if whatswrongfire==("Y"):
+    print("The flame says, 'I'm hungry and I don't have any food to help me grow strong. I need gas...'")
---
+++ functionslol.py
@@ -0,0 +1,7 @@
+x=int()
+y=int()
+def chicken(x,y):
+    x=int(input("First number ya wanna add? "))
+    y=int(input("Next number ya wanna add? "))
+    print(x+y)
+chicken(x,y)
